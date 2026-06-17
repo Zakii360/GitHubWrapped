5e6c2305d077c07ed095b1a7a9bfd3dc16ed61e9
Zakii <zakii@360-search.com>
2026-06-17 16:29:50 -0400

    Refactor fetch calls to reduce variable usage

--- github.js
+++ github.js
@@ -1,11 +1,9 @@
 const BASE = "https://api.github.com";
 
 async function getUser(username) {
-  const res = await fetch(`${BASE}/users/${username}`);
-  return res.json();
+  return (await fetch(`${BASE}/users/${username}`)).json();
 }
 
 async function getRepos(username) {
-  const res = await fetch(`${BASE}/users/${username}/repos?per_page=100`);
-  return res.json();
+  return (await fetch(`${BASE}/users/${username}/repos?per_page=100`)).json();
 }
